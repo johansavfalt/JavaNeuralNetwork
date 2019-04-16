c95ea26bba496701af512c22a5f1995c2724f9a3
Johan Sävfält <johan.savfalt@seal-software.com>
2019-04-16 12:51:01 +0200

    initial commit

---
+++ src/se/johansavfalt/JavaNeuralNetwork/NeuralLayer.java
@@ -0,0 +1,58 @@
+package se.johansavfalt.JavaNeuralNetwork;
+
+
+
+public class NeuralLayer {
+
+	
+	public static double[][] matrixMultiplication(double[][] matrixA, double[][] matrixB){
+		
+		int matrixA_rows = matrixA.length;
+		int matrixA_cols = matrixA[0].length;
+		int matrixB_rows = matrixB.length;
+		int matrixB_cols = matrixB[0].length;
+		
+		
+		
+		double[][] result = new double[matrixA_rows][matrixB_cols]; 
+		
+		
+		if (matrixA_cols != matrixB_rows) {
+			System.out.println("error wrong dimensions of the matrix");
+		} else {
+			
+			for (int i = 0; i < matrixA_rows; i++) {
+				for (int j = 0; j < matrixB_cols; j++) {
+					for (int k = 0; k < matrixA_cols; k++) {
+						result[i][j] += matrixA[i][k] * matrixB[k][j];
+						
+					}
+				}
+				
+			}
+			
+			
+		}
+		
+		return result;
+		
+
+	}
+	
+	public static void main(String[] args) {
+
+		double[][] matrixA = {{1, 2, 3}, {4, 5, 6}};
+		double[][] matrixB = {{1}, {4}, {4}};
+		
+		
+		double[][] matrixC = matrixMultiplication(matrixA, matrixB);
+		
+		for (int i = 0; i < matrixC.length; i++) {
+			for (int j = 0; j < matrixC[i].length; j++) {
+				System.out.println(matrixC[i][j]);
+			}
+		}
+		
+	}
+
+}
